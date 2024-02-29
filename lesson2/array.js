60ff35aa2a2558bcfd5fe394d4873977a4c00abc
Adnan7209 <mdadnan28713@gmail.com>
2024-02-29 20:55:47 +0530

    arrays  done

---
+++ lesson2/array.js
@@ -0,0 +1,12 @@
+var superHeros = [];
+// const heroPowers:number[] = [];
+var heroPowers = [];
+var allUsers = [];
+var MLModels = [
+    [1, 2, 3],
+    [255, 255, 255],
+    []
+];
+superHeros.push("ironman");
+heroPowers.push(100);
+allUsers.push({ name: "", isActive: false });
---
+++ lesson2/array.ts
@@ -0,0 +1,19 @@
+const superHeros:string[] = [];
+// const heroPowers:number[] = [];
+const heroPowers:Array<number> = [];
+
+type User = {
+    name:string,
+    isActive:boolean,
+};
+const allUsers: User[] = [];
+
+const MLModels : number[][]= [
+    [1,2,3],
+    [255,255,255],
+    []
+]
+
+superHeros.push("ironman");
+heroPowers.push(100);
+allUsers.push({name:"",isActive:false});
