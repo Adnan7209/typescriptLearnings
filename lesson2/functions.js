8a2b77c259c9d2af264993dac0bfbd5c9b9134b4
Adnan7209 <mdadnan28713@gmail.com>
2024-02-29 20:10:55 +0530

    functions.js lesson2 done

--- lesson2/functions.js
+++ lesson2/functions.js
@@ -10,7 +10,26 @@ function signUp(name, email, isPass) { }
 var loginUser = function (name, email, isPass) {
     if (isPass === void 0) { isPass = false; }
 };
-addTwo(5);
+var res = addTwo(5);
 getUpper("hello");
 signUp("adnan", "add@gmail.com", false);
 loginUser("adnan", "@gmail.com");
+function getValue(myVal) {
+    if (myVal > 5)
+        return true;
+    return "status OK";
+}
+var getHello = function (s) {
+    return "";
+};
+var heros = ["spiderman", "batman", "ironman"];
+// const heros = [1,2,3,"123",true];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- lesson2/functions.ts
+++ lesson2/functions.ts
@@ -20,6 +20,35 @@ signUp("adnan","add@gmail.com",false);
 loginUser("adnan","@gmail.com");
 
 
+function getValue(myVal:number){
+    if(myVal>5)
+    return true;
+    
+    return "status OK";
+
+}
+
+const getHello = (s:string):string => {
+    return "";
+}
+
+const heros = ["spiderman","batman","ironman"];
+// const heros = [1,2,3,"123",true];
+
+heros.map((hero):string=>{
+    return `hero is ${hero}`
+});
+
+function consoleError(errmsg:string):void{
+    console.log(errmsg);
+}
+
+
+function handleError(errmsg:string):never{
+    throw new Error(errmsg);
+}
+
+
 
 
 
